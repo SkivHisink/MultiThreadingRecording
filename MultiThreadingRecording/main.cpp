ffebd2e91b0c048ec109fd465de3538fd063b5b1
skiv hisink <skiv27rus@gmail.com>
2021-01-03 21:03:50 +0700

    threading test added

--- MultiThreadingRecording/main.cpp
+++ MultiThreadingRecording/main.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
 #include <Windows.h>
+#include <iostream>
 
-BOOL CALLBACK speichereFenster(HWND hwnd, LPARAM lParam) {
+using namespace std;
+using namespace cv;
+
+BOOL CALLBACK getOpenWindowsNames(HWND hwnd, LPARAM lParam) {
 	const DWORD TITLE_SIZE = 1024;
 	WCHAR windowTitle[TITLE_SIZE];
 
@@ -24,15 +27,25 @@ BOOL CALLBACK speichereFenster(HWND hwnd, LPARAM lParam) {
 	return TRUE;
 }
 
-cv::Mat hwnd2mat(HWND hwnd)
+class Hwnd2Mat final
 {
-	HDC hwindowDC, hwindowCompatibleDC;
+public:
+	Hwnd2Mat(HWND hwindow, float scale = 1);
+	virtual ~Hwnd2Mat();
+	virtual void read();
+	Mat image;
 
+private:
+	HWND hwnd;
+	HDC hwindowDC, hwindowCompatibleDC;
 	int height, width, srcheight, srcwidth;
 	HBITMAP hbwindow;
-	cv::Mat src;
 	BITMAPINFOHEADER  bi;
-	HBITMAP bi2;
+};
+
+Hwnd2Mat::Hwnd2Mat(HWND hwindow, float scale)
+{
+	hwnd = hwindow;
 	hwindowDC = GetDC(hwnd);
 	hwindowCompatibleDC = CreateCompatibleDC(hwindowDC);
 	SetStretchBltMode(hwindowCompatibleDC, COLORONCOLOR);
@@ -42,8 +55,10 @@ cv::Mat hwnd2mat(HWND hwnd)
 
 	srcheight = windowsize.bottom;
 	srcwidth = windowsize.right;
-	height = windowsize.bottom / 1;  //change this to whatever size you want to resize to
-	width = windowsize.right / 1;
+	height = (int)(windowsize.bottom * scale);
+	width = (int)(windowsize.right * scale);
+
+	image.create(height, width, CV_8UC4);
 
 	// create a bitmap
 	hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
@@ -54,52 +69,126 @@ cv::Mat hwnd2mat(HWND hwnd)
 	bi.biBitCount = 32;
 	bi.biCompression = BI_RGB;
 	bi.biSizeImage = 0;
-	bi.biXPelsPerMeter = 1;
-	bi.biYPelsPerMeter = 2;
-	bi.biClrUsed = 3;
-	bi.biClrImportant = 4;
+	bi.biXPelsPerMeter = 0;
+	bi.biYPelsPerMeter = 0;
+	bi.biClrUsed = 0;
+	bi.biClrImportant = 0;
 
 	// use the previously created device context with the bitmap
 	SelectObject(hwindowCompatibleDC, hbwindow);
+	read();
+};
+
+void Hwnd2Mat::read()
+{
 	// copy from the window device context to the bitmap device context
 	StretchBlt(hwindowCompatibleDC, 0, 0, width, height, hwindowDC, 0, 0, srcwidth, srcheight, SRCCOPY); //change SRCCOPY to NOTSRCCOPY for wacky colors !
+	GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, image.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);  //copy from hwindowCompatibleDC to hbwindow
+};
 
-	src.create(height, width, CV_8UC4);
-	GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, src.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);  //copy from hwindowCompatibleDC to hbwindow
-
+Hwnd2Mat::~Hwnd2Mat()
+{
 	// avoid memory leak
 	DeleteObject(hbwindow);
 	DeleteDC(hwindowCompatibleDC);
 	ReleaseDC(hwnd, hwindowDC);
+};
+class VideoWrite
+{
+public:
+	static mutex mtx;
+	static void run(string filename, HWND hwndWindow)
+	{
+		Mat ui;
+		ui = Mat(40, 400, CV_8UC3, Scalar(0, 130, 0));
+		putText(ui, "Press Esc to stop capturing", Point(30, 30), FONT_HERSHEY_COMPLEX, 0.7,
+			Scalar(0, 0, 255), 1);
 
-	return src;
-}
+		Mat bgrImg;
 
-int main(int argc, char** argv)
+		Hwnd2Mat capDesktop(hwndWindow/*GetDesktopWindow()*/);
+		VideoWriter writer;
+		int codec = VideoWriter::fourcc('X', 'V', 'I', 'D');
+		double fps = 30.0;
+		//string filename = "./desktop_capture.avi";
+		writer.open(filename, codec, fps, capDesktop.image.size(), true);
+		// check if we succeeded
+		if (!writer.isOpened()) {
+			cerr << "Could not open the output video file for write\n";
+			return /*-1*/;
+		}
+		while (true)
+		{
+			capDesktop.read();
+			cvtColor(capDesktop.image, bgrImg, COLOR_BGRA2BGR);
+			writer << bgrImg;
+			//imshow(filename/*"desktop capture"*/, ui);
+		//	int key = waitKey(5);
+
+		//	if (key == 27)
+			//	break;
+		}
+	}
+};
+int main()
 {
 	std::vector<std::wstring> titles; // we use std::wstring in place of std::string. This is necessary so that the entire character set can be represented.
-	EnumWindows(speichereFenster, reinterpret_cast<LPARAM>(&titles));
+	EnumWindows(getOpenWindowsNames, reinterpret_cast<LPARAM>(&titles));
 	HWND hwndDesktop = GetDesktopWindow();
 	size_t number = 0;
 	int i = 0;
-	for (const auto& title : titles)
-		std::wcout << L"Title: " << i++ << title << std::endl;
-	std::cin >> number;
-	HWND hwndWindow = FindWindow(NULL, titles[number].c_str());
-	cv::namedWindow("output", cv::WINDOW_NORMAL);
-	cv::Mat src = hwnd2mat(/*hwndDesktop*/hwndWindow);
-	cv::VideoWriter outputVideo("output.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 1, cv::Size(src.cols, src.rows));
-	outputVideo.write(src);
-	int key = 0;
-	while (key != 27)
-	{
-		 src = hwnd2mat(/*hwndDesktop*/hwndWindow);
-		 outputVideo.write(src);
-		// you can do some image processing here
-		cv::imshow("output", src);
-		//cv::imshow("desktop", hwnd2mat(hwndDesktop));
-		key = cv::waitKey(60); // you can change wait time
+	for (const auto& title : titles) {
+		std::wcout << i++ << " " << title << std::endl;
 	}
-	outputVideo.release();
+	std::cin >> number;
+	//HWND hwndWindow = FindWindow(NULL, titles[number].c_str());
+	thread kek(&VideoWrite::run, "./desktop_capture.avi", hwndDesktop/*hwndWindow*/);
+	thread kek2(&VideoWrite::run, "./desktop_capture2.avi", FindWindow(NULL, titles[number + 1].c_str()));
+	bool start = false;
+	Mat ui;
+	ui = Mat(40, 400, CV_8UC3, Scalar(0, 130, 0));
+	putText(ui, "Press Esc to stop capturing", Point(30, 30), FONT_HERSHEY_COMPLEX, 0.7,
+		Scalar(0, 0, 255), 1);
+	while (true) {
+		if (!start) {
+			kek.detach();
+			kek2.detach();
+			start = true;
+		}
+		imshow("filename"/*"desktop capture"*/, ui);
+		int key = waitKey(5);
+
+		if (key == 27)
+			break;
+	}
+
+	//Mat bgrImg;
+	//Hwnd2Mat capDesktop(hwndWindow/*GetDesktopWindow()*/);
+	//Mat ui;
+	//ui = Mat(40, 400, CV_8UC3, Scalar(0, 130, 0));
+	//putText(ui, "Press Esc to stop capturing", Point(30, 30), FONT_HERSHEY_COMPLEX, 0.7,
+	//	Scalar(0, 0, 255), 1);
+	//VideoWriter writer;
+	//int codec = VideoWriter::fourcc('X', 'V', 'I', 'D');
+	//double fps = 30.0;
+	//string filename = "./desktop_capture.avi";
+	//writer.open(filename, codec, fps, capDesktop.image.size(), true);
+	//// check if we succeeded
+	//if (!writer.isOpened()) {
+	//	cerr << "Could not open the output video file for write\n";
+	//	return -1;
+	//}
+
+	//while (true)
+	//{
+	//	capDesktop.read();
+	//	cvtColor(capDesktop.image, bgrImg, COLOR_BGRA2BGR);
+	//	writer << bgrImg;
+	//	imshow("desktop capture", ui);
+	//	int key = waitKey(5);
+
+	//	if (key == 27)
+	//		break;
+	//}
 	return 0;
 }
